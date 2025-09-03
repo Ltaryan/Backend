ef3b6f682f5b078f626b909afd312550ca695296
Ltaryan <aryansingh7462856900@gmail.com>
2025-09-03 17:06:06 +0530

    Dev 1: Added login, auth, logout APIs

---
+++ src/test/java/com/HdfcLife/SecureAuthService/AuthService/HdfcLifeSecureAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HdfcLife.SecureAuthService.AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfcLifeSecureAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
